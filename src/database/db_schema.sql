f61381a934a67d3fc56dc05bda4b91fe8cfe1e3e
Claudio Scafesi <scafesi@gmail.com>
2018-03-26 16:30:50 +0200

    db schema and data setup

--- src/database/db_schema.sql
+++ src/database/db_schema.sql
@@ -1,18 +1,20 @@
 BEGIN;
 
-DROP TABLE IF EXISTS rates, currencies;
+DROP TABLE IF EXISTS bookmarks, users;
 
-CREATE TABLE currencies (
-  id int NOT NULL,
-  name varchar(255) NOT NULL,
-  code varchar(255) NOT NULL
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
 );
 
-CREATE TABLE rates (
-  fromcurrency_id int NOT NULL,
-  tocurrency_id int NOT NULL,
-  rate float NOT NULL,
-  timestamp timestamp NOT NULL
+CREATE TABLE bookmarks (
+  user_id INTEGER NOT NULL REFERENCES users(user_id),
+  already_known integer[],
+  interested_in integer[],
+  starred integer[]
 );
+
 
 COMMIT;
